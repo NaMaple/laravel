a28b83c3040e88843b658dc08013c9e43e2e01b3
yintianxiong <yintx_129@163.com>
2020-05-16 04:39:56 +0800

    web route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', 'UserController@index');
+Route::post('/user/add', 'UserController@add');
+
+Route::get('/test', function () {
+    echo 'test';
+});
+
+Route::post('/test/post', function () {
+    echo 'test post';
+});
