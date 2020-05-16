bbe2bbf24d712049c287fcbde487418049be9aef
yintianxiong <yintx_129@163.com>
2020-05-16 13:54:07 +0800

    redis set

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\Http\Resources\Product as ProductResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class ProductController extends Controller
 {
     public function show ($id)
     {
         return new ProductResource(Product::find($id));
+    }
+
+    public function add(Request $request) {
+        $name = $request->input('name');
+        var_dump($name);
+        Redis::set('name', $name);
+        return Redis::get('name');
     }
 }
